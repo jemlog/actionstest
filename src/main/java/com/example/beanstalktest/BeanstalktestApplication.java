b09b1650869390278ce6d8c096e74a7be17274b8
jemin <jemin03120111@gmail.com>
2022-09-17 16:32:39 +0900

    .

---
+++ src/main/java/com/example/beanstalktest/BeanstalktestApplication.java
@@ -0,0 +1,13 @@
+package com.example.beanstalktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanstalktestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeanstalktestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/beanstalktest/BeanstalktestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beanstalktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanstalktestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
